651a05547c17c36993265a20564a99269766f76c
Eriqxx <erauws@gmail.com>
2023-11-10 11:42:42 +0100

    cardsclub css ja of nee

---
+++ Test.js
@@ -0,0 +1,14 @@
+console.log("ja ja, werkt dit?");
+
+console.log("Hallo daar!");
+console.log("Ik zie dat je de console.log functie aan het uitproberen bent!");
+console.log("Zal ik een figuurtje voor je maken?");
+console.log("1, 2, 3...")
+console.log("*****");
+console.log(" ***");
+console.log("  *");
+console.log("  *");
+console.log("  *");
+console.log("  *");
+console.log("  *");
+
